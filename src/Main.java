4e73d6e118db1798e08f76bdf043fc8285a1db5d
Kyaw Soe <kyawsh@m1.com.sg>
2023-03-02 22:31:51 +0800

    Added Singly Linked List

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Array arr = new Array();
-        arr.add(60);
-        arr.add(80);
-        arr.remove(2);
+//        Array arr = new Array();
+//        arr.add(60);
+//        arr.add(80);
+//        arr.remove(2);
+
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.pushFront(20);
+        list.pushFront(40);
+        list.pushFront(60);
+        list.printList();
+        list.topFront();
+        list.pushBack(80);
+        list.pushBack(90);
+        list.topBack();
+        list.popFront();
+        list.findNode(100);
+        list.popBack();
     }
 }
---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,115 @@
+public class SinglyLinkedList {
+    private Node head = null;
+
+    private class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node getTheLastNode(){
+        Node node = head;
+
+        if (node != null){
+            while (node.next != null){
+                node = node.next;
+            }
+        }
+
+        return node;
+    }
+
+    public void pushFront(int value){
+        System.out.println("Pushed front");
+        Node newNode = new Node(value);
+
+        if (head == null){
+            head = newNode;
+            newNode.next = null;
+        } else {
+            Node temp = head;
+            head = newNode;
+            head.next = temp;
+        }
+    }
+
+    public void topFront(){
+        System.out.println("Top Front is " + head.value);
+    }
+
+    public void popFront(){
+        System.out.println("Popped front");
+        head = head.next;
+        printList();
+    }
+
+    public void popBack(){
+        System.out.println("Popped Back");
+        Node node = head;
+        Node prevNode = head;
+
+        if (node != null){
+            while (node.next != null){
+                prevNode = node;
+                node = node.next;
+            }
+            prevNode.next = null;
+        }
+        printList();
+    }
+
+    public void findNode(int value){
+        System.out.println("Finding Node");
+        Node node = head;
+        boolean found = false;
+
+        if (node != null){
+            while (node.next != null){
+                if (node.value == value){
+                    System.out.println("Found the value");
+                    found = true;
+                    break;
+                }
+                node = node.next;
+            }
+
+            if (!found){
+                if (node.value == value){
+                    System.out.println("Found the value");
+                } else {
+                    System.out.println("Didn't find the value");
+                }
+            }
+        }
+    }
+
+    public void pushBack(int value){
+        System.out.println("Pushed back");
+        Node newNode = new Node(value);
+        newNode.next = null;
+
+        getTheLastNode().next = newNode;
+        printList();
+    }
+
+    public void topBack(){
+        System.out.println("Top Back is " + getTheLastNode().value);
+    }
+
+    public void printList(){
+        Node node = head;
+
+        System.out.print("List is ");
+        if (node != null){
+            while (node.next != null){
+                System.out.print(node.value + ", ");
+                node = node.next;
+            }
+            System.out.println(node.value);
+        }
+    }
+}
